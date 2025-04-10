0b51f920a703a61b0c35992eb9f03650094e5a8a
cekwedike <c.ekwedike@alustudent.com>
2025-04-10 23:27:01 +0200

    Add react-router-dom for routing support and wrap App component with BrowserRouter for improved navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { TodoProvider } from './context/TodoContext';
 import { MainLayout } from './components/layout/MainLayout';
 import { TodoList } from './components/todo/TodoList';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <TodoProvider>
-      <MainLayout>
-        <TodoList />
-      </MainLayout>
-    </TodoProvider>
+    <BrowserRouter>
+      <TodoProvider>
+        <MainLayout>
+          <TodoList />
+        </MainLayout>
+      </TodoProvider>
+    </BrowserRouter>
   );
 }
 
